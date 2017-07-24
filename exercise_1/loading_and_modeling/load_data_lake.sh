1fb0327a840f0bd95c7ce453a1e2a27aa9570810
hsuthar1 <himal.suthar@berkeley.edu>
2017-07-23 23:21:36 -0700

    refactoring

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -20,13 +20,19 @@ unzip medicare_data.zip
 # create our hdfs directory
 hdfs dfs -mkdir /user/w205/hospital_compare
 
+hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir /user/w205/hospital_compare/Measures
+hdfs dfs -mkdir /user/w205/hospital_compare/surveys_responses
+
 # remove first line of files and rename
 OLD_FILE="Hospital General Information.csv"
 NEW_FILE="hospitals.csv"
 tail -n +2 "$OLD_FILE" > $NEW_FILE
 
 # copy files to hdfs
-hdfs dfs -put $NEW_FILE /user/w205/hospital_compare
+hdfs dfs -put $NEW_FILE /user/w205/hospital_compare/hospitals
 
 # remove first line of files and rename
 OLD_FILE="Timely and Effective Care - Hospital.csv"
@@ -34,7 +40,7 @@ NEW_FILE="effective_care.csv"
 tail -n +2 "$OLD_FILE" > $NEW_FILE
 
 # copy files to hdfs
-hdfs dfs -put $NEW_FILE /user/w205/hospital_compare
+hdfs dfs -put $NEW_FILE /user/w205/hospital_compare/effective_care
 
 # remove first line of files and rename
 OLD_FILE="Readmissions and Deaths - Hospital.csv"
@@ -42,7 +48,7 @@ NEW_FILE="readmissions.csv"
 tail -n +2 "$OLD_FILE" > $NEW_FILE
 
 # copy files to hdfs
-hdfs dfs -put $NEW_FILE /user/w205/hospital_compare
+hdfs dfs -put $NEW_FILE /user/w205/hospital_compare/readmissions
 
 # remove first line of files and rename
 OLD_FILE="Measure Dates.csv"
@@ -50,7 +56,7 @@ NEW_FILE="Measures.csv"
 tail -n +2 "$OLD_FILE" > $NEW_FILE
 
 # copy files to hdfs
-hdfs dfs -put $NEW_FILE /user/w205/hospital_compare
+hdfs dfs -put $NEW_FILE /user/w205/hospital_compare/Measures
 
 # remove first line of files and rename
 OLD_FILE="hvbp_hcahps_11_10_2016.csv"
@@ -58,7 +64,7 @@ NEW_FILE="surveys_responses.csv"
 tail -n +2 "$OLD_FILE" > $NEW_FILE
 
 # copy files to hdfs
-hdfs dfs -put $NEW_FILE /user/w205/hospital_compare
+hdfs dfs -put $NEW_FILE /user/w205/hospital_compare/surveys_responses
 
 # change directory back to original
 cd $MY_CWD
